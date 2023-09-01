01bd60e46c19abe5bca5b901cd7d86d21472d477
jhoisz <jvrda@ufpi.edu.br>
2023-09-01 19:44:25 -0300

    Created MobX Store

---
+++ lib/data/store/home_store.dart
@@ -0,0 +1,44 @@
+import 'package:mobx/mobx.dart';
+
+import '../../controllers/article_dao_controller.dart';
+import '../../domain/entities/article.dart';
+
+part 'home_store.g.dart';
+
+class HomeStore extends _HomeStoreBase with _$HomeStore {}
+
+abstract class _HomeStoreBase with Store {
+  final DaoController daoController = DaoController();
+  ObservableList<Article> favoritesList = ObservableList<Article>();
+
+  _HomeStoreBase() {
+    getFavorites();
+  }
+
+  @action
+  Future<void> getFavorites() async {
+    favoritesList = (await daoController.getArticles()).asObservable();
+  }
+
+  @action
+  void setFavorite(Article article) {
+    favoritesList.add(article);
+
+    daoController.addArticle(
+      article: article,
+    );
+  }
+
+  @action
+  void removeFavorite(Article article) {
+    favoritesList.removeWhere((e) => e.id == article.id);
+
+    daoController.removeArticle(
+      article: article,
+    );
+  }
+
+  bool isFavorite(int id) {
+    return favoritesList.any((e) => e.id == id);
+  }
+}
